060146b47c2ee9953d80b5596d285433d89dc5d3
nachof948 <nachof948@gmail.com>
2024-04-27 11:24:40 -0300

    Cambios

---
+++ Frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "inmobi-estate.firebaseapp.com",
+  projectId: "inmobi-estate",
+  storageBucket: "inmobi-estate.appspot.com",
+  messagingSenderId: "384787935996",
+  appId: "1:384787935996:web:948af484d34a31fab65d79"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
