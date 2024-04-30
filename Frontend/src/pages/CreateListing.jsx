a9c60c576f8d1ce4c5d674752380259d2d7893af
nachof948 <nachof948@gmail.com>
2024-04-30 10:17:13 -0300

    Cambios en el codigo de publica una vivienda

--- Frontend/src/pages/CreateListing.jsx
+++ Frontend/src/pages/CreateListing.jsx
@@ -1,6 +1,4 @@
-import { getDownloadURL, getStorage, uploadBytesResumable, ref } from "firebase/storage"
-import { useState, useEffect } from "react"
-import { app } from "../firebase"
+import { useState } from "react"
 import { UploadImageList } from "../components/UploadImageList"
 
 
