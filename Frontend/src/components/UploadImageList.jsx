dfb6fabfc268e688361f1ca8d14afa5673914eb6
nachof948 <nachof948@gmail.com>
2024-04-30 10:16:34 -0300

    Seccion publicar inmueble

---
+++ Frontend/src/components/UploadImageList.jsx
@@ -0,0 +1,105 @@
+import { getDownloadURL, getStorage, uploadBytesResumable, ref } from "firebase/storage";
+import { useState } from "react";
+import { app } from "../firebase";
+
+const UploadImageList = ({ onImageChange }) => {
+  const [images, setImages] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  console.log(imageUrls)
+
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    if (images.length > 0 && images.length < 7) {
+      setCargando(true);
+      const promises = [];
+      for (let i = 0; i < images.length; i++) {
+        promises.push(storeImage(images[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+        setImageUrls([...imageUrls, ...urls]);
+        onImageChange([...imageUrls, ...urls]);
+        setImageUploadError(false);
+        setCargando(false);
+        })
+        .catch((error) => {
+          setImageUploadError("Fallo la carga de las imagenes");
+        });
+    } else {
+      setImageUploadError("Solo puedes subir 6 imagenes");
+    }
+  };
+
+  const storeImage = async (image) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const imageName = new Date().getTime() + "_" + image.name;
+      const storageRef = ref(storage, imageName);
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on(
+        "state_change",
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref)
+            .then((downloadURL) => {
+              resolve(downloadURL);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setImageUrls(imageUrls.filter((_, i) => i !== index));
+  };
+
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        <input
+          className="p-3 border border-gray-300 rounded w-full"
+          type="file"
+          name="images"
+          accept="image/*"
+          multiple
+          onChange={(e) => setImages(e.target.files)}
+        />
+        <button
+          type="button"
+          disabled={cargando}
+          onClick={handleImageSubmit}
+          className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+        >
+          {cargando ? "Subiendo..." : "Subir"}
+        </button>
+      </div>
+      <p className="text-red-700">{imageUploadError && imageUploadError}</p>
+      {imageUrls.length > 0 &&
+        imageUrls.map((url, index) => (
+          <div className="flex items-center justify-between p-3 border border-slate-300 rounded-lg" key={index}>
+            <img src={url} alt="Casa publicada" className="h-24 object-contain rounded-lg" />
+            <button
+              type="button"
+              onClick={() => handleRemoveImage(index)}
+              className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+            >
+              Eliminar
+            </button>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export { UploadImageList };
--- Frontend/src/pages/CreateListing.jsx
+++ Frontend/src/pages/CreateListing.jsx
@@ -1,6 +1,21 @@
+import { getDownloadURL, getStorage, uploadBytesResumable, ref } from "firebase/storage"
+import { useState, useEffect } from "react"
+import { app } from "../firebase"
+import { UploadImageList } from "../components/UploadImageList"
 
 
 const CreateListing = () => {
+  const checkboxStyle = 'w-5 h-5 cursor-pointer'
+  const spanStyle = 'font-semibold text-lg'
+  const [formData, setFormData] = useState({
+    imagesUrls:[],
+  })
+  
+  const handleImageChange = (imagesUrls) =>{
+    setFormData({...formData, imagesUrls: imagesUrls})
+  }
+  console.log(formData)
+
   return(
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Publica una propiedad</h1>
@@ -11,46 +26,46 @@ const CreateListing = () => {
           <input type="text" name="address" placeholder="Direccion" className="border p-3 rounded-lg" maxLength={'62'} minLength={'10'} required />
           <div className="flex items-center gap-6 flex-wrap">
             <div className="flex gap-2 items-center">
-              <input type="checkbox" name="sale" className="w-5 h-5" />
-              <span className="font-semibold text-lg">Vender</span>
+              <input type="checkbox" name="sale" className={checkboxStyle} />
+              <span className={spanStyle}>Vender</span>
             </div>
             <div className="flex gap-2 items-center">
-              <input type="checkbox" name="rent" className="w-5 h-5" />
-              <span className="font-semibold text-lg">Alquilar</span>
+              <input type="checkbox" name="rent" className={checkboxStyle} />
+              <span className={spanStyle}>Alquilar</span>
             </div>
             <div className="flex gap-2 items-center">
-              <input type="checkbox" name="parking" className="w-5 h-5" />
-              <span className="font-semibold text-lg">Estacionamiento</span>
+              <input type="checkbox" name="parking" className={checkboxStyle} />
+              <span className={spanStyle}>Estacionamiento</span>
             </div>
             <div className="flex gap-2 items-center">
-              <input type="checkbox" name="furnished" className="w-5 h-5" />
-              <span className="font-semibold text-lg">Amueblado</span>
+              <input type="checkbox" name="furnished" className={checkboxStyle} />
+              <span className={spanStyle}>Amueblado</span>
             </div>
             <div className="flex gap-2 items-center">
-              <input type="checkbox" name="offer" className="w-5 h-5" />
-              <span className="font-semibold text-lg">Oferta</span>
+              <input type="checkbox" name="offer" className={checkboxStyle} />
+              <span className={spanStyle}>Oferta</span>
             </div>
           </div>
           <div className=" flex items-center gap-6 flex-wrap">
             <div className=" flex items-center gap-2">
               <input type="number" name="bedrooms" min={'1'} max={'10'} required className="p-3 border border-gray-300 rounded-lg"/>
-              <span className="font-semibold text-lg">Habitaciones</span>
+              <span className={spanStyle}>Habitaciones</span>
             </div>
             <div className=" flex items-center gap-2">
               <input type="number" name="bathrooms" min={'1'} max={'10'} required className="p-3 border border-gray-300 rounded-lg"/>
-              <span className="font-semibold text-lg">Baños</span>
+              <span className={spanStyle}>Baños</span>
             </div>
             <div className=" flex items-center gap-2">
               <input type="number" name="regularPrice" min={'1'} max={'10'} required className="p-3 border border-gray-300 rounded-lg"/>
               <div className="flex flex-col items-center">
-                <span className="font-semibold text-lg">Precio Regular</span>
+                <span className={spanStyle}>Precio Regular</span>
                 <span className="text-xs">($ / Mes)</span>
               </div>
             </div>
             <div className=" flex items-center gap-2">
               <input type="number" name="discountPrice" min={'1'} max={'10'} required className="p-3 border border-gray-300 rounded-lg"/>
               <div className="flex flex-col items-center">
-                <span className="font-semibold text-lg">Precio Descontado</span>
+                <span className={spanStyle}>Precio Descontado</span>
                 <span className="text-xs">($ / Mes)</span>
               </div>
             </div>
@@ -60,10 +75,7 @@ const CreateListing = () => {
           <p className="font-semibold">Imagenes:
           <span className="font-normal text-gray-600 ml-2">La primera imagen será la portada (max 6).</span>
           </p>
-          <div className="flex items-center gap-4">
-            <input className="p-3 border border-gray-300 rounded w-full" type="file" name="images" accept="image/*" multiple />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Subir</button>
-          </div>
+          <UploadImageList onImageChange={handleImageChange} />
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Publicar la propiedad</button>
         </div>
       </form>
